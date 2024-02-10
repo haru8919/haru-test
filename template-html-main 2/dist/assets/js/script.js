37997eb221bf939e0e54256ba4ee2acd7ec0376d
haru8919 <109137337+haru8919@users.noreply.github.com>
2024-02-11 00:03:36 +0900

    Add files via upload

---
+++ template-html-main 2/dist/assets/js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+jQuery(function ($) {
+  $(".js-hamburger, .js-drawer").on("click", function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").toggleClass("is-open");
+  });
+});
+
+// スライダー
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  effect: "fade",
+  speed: 3000,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 3000
+  }
+});
+
+// const Swiper = new Swiper(".swiper2", {
+//   loop: true,
+//   slidesPerView: "auto",
+//   spaceBetween: 18,
+//   grabCursor: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   breakpoints: {
+//     1025: {
+//       spaceBetween: 32,
+//     },
+//   },
+// });
+
+var swiper2 = new Swiper(".swiper2", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 18,
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    1025: {
+      spaceBetween: 32
+    }
+  }
+});
